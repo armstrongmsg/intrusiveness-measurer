0053554b544cafc4947d4e04f644e1db3783b761
goes.armstrong151@gmail.com <goes.armstrong151@gmail.com@3978af62-aba8-0a5b-03df-e2cee233907d>
2012-11-29 18:49:44 +0000

    Task #2069 Write scripts to collect MapReduce benchmarks data
    - The TIME_BETWEEN_CHECKS argument is now working correctly.
    - The program is getting the CPU and memory consumption correctly.
    - Added debug features to the program.

--- trunk/IntrusivenessMeasurer/data_collector/slave_data_collector.sh
+++ trunk/IntrusivenessMeasurer/data_collector/slave_data_collector.sh
@@ -21,9 +21,12 @@
 # PROCESS_NAME : the name of the process to be monitored  
 # TIME_BETWEEN_CHECKS : time between data collects.
 # OUTPUT_BASE_FILENAME : this radical is used to construct the output file names. 
-#The program creates two files, one for CPU information and other for memory information. 
-#If OUTPUT_BASE_FILENAME is "aaaa", the created files are aaaa.cpu and aaaa.mem
+# The program creates two files, one for CPU information and other for memory information. 
+# If OUTPUT_BASE_FILENAME is "aaaa", the created files are aaaa.cpu and aaaa.mem
 #  
+
+# TODO Arguments checking
+# TODO Error handling
 
 PROCESS_NAME=$1
 TIME_BETWEEN_CHECKS=$2
@@ -31,9 +34,28 @@ OUTPUT_BASE_FILENAME=$3
 OUTPUT_CPU_FILENAME="$OUTPUT_BASE_FILENAME.cpu"
 OUTPUT_MEMORY_FILENAME="$OUTPUT_BASE_FILENAME.mem"
 
+DEBUG=true
+DEBUG_FILE_NAME="collector.log"
+
+function debug_startup
+{
+	if [ $DEBUG ]; then
+		touch $DEBUG_FILE_NAME	
+	fi
+}
+
+function debug
+{
+	if [ $DEBUG ]; then
+		# TODO if the log file is too big, it must truncate to 0
+		# or do something so the file does not grow without limit.
+		echo $1	>> $DEBUG_FILE_NAME
+	fi
+}
+
 function get_process_pid
 {
-	echo "`ps axco pid,command | grep $PROCESS_NAME | grep -v grep | cut -d " " -f -1`" 
+	echo "`ps axco pid,command | grep $PROCESS_NAME | cut -d " " -f -2`" 
 }
 
 function process_is_running
@@ -41,7 +63,7 @@ function process_is_running
 	# I think this is not enough.
 	# the process name is not enough to identify the Hadoop task.
 	# so I should consider the user and the group.
-	if [ ! "`ps axco command | grep $PROCESS_NAME | grep -v grep`" = "" ] ; then
+	if [ ! "`ps axco command | grep $PROCESS_NAME`" = "" ] ; then
 		echo "1"
 	else
 		echo "0"
@@ -50,14 +72,12 @@ function process_is_running
 
 function get_cpu_consumption
 {
-	# TODO TO BE IMPLEMENTED
-	echo "1";
+	echo "`ps -p $(get_process_pid) -o %cpu | sed 1d`"
 }
 
 function get_memory_consumption
 {
-	# TODO TO BE IMPLEMENTED
-	echo "1";
+	echo "`ps -p $(get_process_pid) -o %mem | sed 1d`"
 }
 
 function write_cpu_consumption
@@ -82,21 +102,31 @@ function write_file_ending
 	echo "stop time=`date "+%d-%m-%Y-%H-%M-%S"`" >> "$1"
 }
 
+debug_startup
+
 touch $OUTPUT_CPU_FILENAME
 touch $OUTPUT_MEMORY_FILENAME
+
+debug "created output files"
 
 write_file_header $OUTPUT_CPU_FILENAME
 write_file_header $OUTPUT_MEMORY_FILENAME
 
+debug "wrote header to output files"
+
 while [ $(process_is_running) -eq 1 ]; do
 
 	CPU_CONSUMPTION=$(get_cpu_consumption)
-	MEMORY_CONSUMPTION=$(get_memory_consumption)
+	MEMORY_CONSUMPTION=$(get_memory_consumption) 
 
 	write_cpu_consumption $CPU_CONSUMPTION
 	write_memory_consumption $MEMORY_CONSUMPTION
+
+	sleep $TIME_BETWEEN_CHECKS
 done
 
 write_file_ending $OUTPUT_CPU_FILENAME
 write_file_ending $OUTPUT_MEMORY_FILENAME
+
+debug "wrote ending of output files"
 
